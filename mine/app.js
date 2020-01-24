52913f8a773e0b4c6edae6c7823eae8c1a3b13d8
TylerBlood <tylerlblood@gmail.com>
2020-01-24 14:06:26 -0500

    completed lesson 16, pipes

--- mine/app.js
+++ mine/app.js
@@ -1,22 +1,17 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
 
-myReadStream.on('data', function(chunk){
-  console.log('new chunk received');
-  myWriteStream.write(chunk);
+
+
+
+var server = http.createServer(function(req, res){
+  console.log(`request was made: ${req.url}`);
+  res.writeHead(200,{'Content-Type': 'text/plain'});
+  var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8');
+  myReadStream.pipe(res);
 });
 
 
-
-// var server = http.createServer(function(req, res){
-//   console.log(`request was made: ${req.url}`);
-//   res.writeHead(200,{'Content-Type': 'text/plain'});
-//   res.end('Hey ninjas!');
-// });
-
-
-// server.listen(3000, '127.0.0.1');
-// console.log('yo dawgs, now listening to port 3000');
+server.listen(3000, '127.0.0.1');
+console.log('now listening to port 3000');
